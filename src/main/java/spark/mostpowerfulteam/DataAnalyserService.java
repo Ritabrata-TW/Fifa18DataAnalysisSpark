ad5259071d46837a7b44fb5089ae4ef1614f87c4
Ritabrata Moitra <rmoitra@thoughtworks.com>
2017-10-31 21:07:17 +0530

    Find top 10 clubs by average rating

--- src/main/java/spark/mostpowerfulteam/DataAnalyserService.java
+++ src/main/java/spark/mostpowerfulteam/DataAnalyserService.java
@@ -6,25 +6,31 @@ import scala.Serializable;
 import scala.Tuple2;
 
 class DataAnalyserService implements Serializable {
-    JavaPairRDD<String, Integer> findMostPowerfulTeam() {
+    JavaPairRDD<String, Float> findMostPowerfulTeam() {
         CsvDataImporter csvDataImporter = new CsvDataImporter();
         JavaRDD<String> rdd = csvDataImporter.importFile("/src/main/resources/CompleteDataset.csv");
-
         JavaRDD<Player> playersRDD = rdd.map(this::mapToPlayerData);
+        JavaPairRDD<String, Player> clubToPlayerMapping = mapClubToPlayer(playersRDD);
 
-        JavaPairRDD<String, Player> clubToPlayerMapping =
-            playersRDD.mapToPair(player -> new Tuple2<>(player.getClub(), player))
-                .filter(v1 -> !v1._1().isEmpty());
+        JavaPairRDD<String, Tuple2<Integer, Integer>> rdd1 = clubToPlayerMapping.mapValues(player -> new Tuple2<>(player.getOverall(), 1))
+            .reduceByKey((v1, v2) -> new Tuple2<>(v1._1() + v2._1(), v1._2() + v2._2()));
 
-        JavaPairRDD<String, Integer> clubToAggregatedOverallMapping =
-            clubToPlayerMapping.aggregateByKey(0, (v1, v2) -> v1 + v2.getOverall(), (v1, v2) -> v1 + v2);
+        JavaPairRDD<String, Float> clubToAverageRating = rdd1.mapToPair(tuple2 ->
+            new Tuple2<>(tuple2._1(), (float) (tuple2._2()._1() / tuple2._2()._2())));
 
-        JavaPairRDD<Integer, String> overallToClubMapping =
-            new SparkRddReverserUtil<Integer, String>().reverseJavaPairRdd(clubToAggregatedOverallMapping);
+        JavaPairRDD<Float, String> sortedAverageRatingToClubMapping = new SparkRddReverserUtil<Float, String>()
+            .reverseJavaPairRdd(clubToAverageRating)
+            .sortByKey(false);
 
-        JavaPairRDD<Integer, String> sortedOverallToClubMapping = overallToClubMapping.sortByKey(false);
+        return new SparkRddReverserUtil<String, Float>().reverseJavaPairRdd(sortedAverageRatingToClubMapping);
+    }
 
-        return new SparkRddReverserUtil<String, Integer>().reverseJavaPairRdd(sortedOverallToClubMapping);
+    private JavaPairRDD<String, Player> mapClubToPlayer(JavaRDD<Player> playersRDD) {
+        return playersRDD.mapToPair(player -> new Tuple2<>(player.getClub(), player))
+            .filter(clubToPairMapping -> {
+                String clubName = clubToPairMapping._1();
+                return !clubName.isEmpty();
+            });
     }
 
     private Player mapToPlayerData(String line) {
--- src/main/java/spark/mostpowerfulteam/Main.java
+++ src/main/java/spark/mostpowerfulteam/Main.java
@@ -5,7 +5,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 public class Main {
     public static void main(String[] args) {
         DataAnalyserService dataAnalyserService = new DataAnalyserService();
-        JavaPairRDD<String, Integer> teamToAggregatedOverallMapping = dataAnalyserService.findMostPowerfulTeam();
+        JavaPairRDD<String, Float> teamToAggregatedOverallMapping = dataAnalyserService.findMostPowerfulTeam();
 
         teamToAggregatedOverallMapping.take(10).forEach(System.out::println);
     }
